6154030fa5e8c7b77d6683b121272ee590a9f0dc
Ali Asghar <aliasgharmandasore@gmail.com>
2025-04-10 12:58:47 +0500

    Basic menu added without functionality just listing.

--- calculator.py
+++ calculator.py
@@ -1,1 +1,2 @@
-print("******Select ")
+print("Select from the following options: ")
+opt = input("= Press 1 to perform addition \n= Press 2 to perform subtraction \n= Press 3 to perform Multiplication \n= Press 4 to perform Division\n>>> ")

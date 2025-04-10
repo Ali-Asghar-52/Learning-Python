e61ef1e9d0967cbcfd26bec279a0abb91300c981
Ali Asghar <aliasgharmandasore@gmail.com>
2025-04-10 12:59:49 +0500

    Basic menu added without functionality just listing.

--- calculator.py
+++ calculator.py
@@ -1,2 +1,15 @@
+def checkNum():
+    
+
+def addition():
+    digit_1 = int(input("Enter first digit: "))
+
+    digit_2 = input("Enter Second digit: ")
+
+
+
 print("Select from the following options: ")
 opt = input("= Press 1 to perform addition \n= Press 2 to perform subtraction \n= Press 3 to perform Multiplication \n= Press 4 to perform Division\n>>> ")
+result = 0
+# match opt:
+#     case 1:
